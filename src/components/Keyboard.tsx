a894c6f743efd7d13222c6b24829372a6de56a28
Milosz <miloszbaranski@outlook.com>
2022-02-05 18:13:53 +0100

    handle keydown event, add class to clicked key

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Keys from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 import "styles/components/Keyboard.scss";
 
 const Keyboard: React.FC = () => {
+  const [keys, updateKeys] = useState<any>([]);
+
+  const initKeysHandler = (keys: {
+    keys: object;
+    keyboardRowsDOM: HTMLDivElement;
+  }) => {
+    const rows = keys.keyboardRowsDOM.childNodes;
+    rows.forEach((row) => {
+      const rowItems = row.childNodes;
+      rowItems.forEach((rowItem) => {
+        updateKeys((oldArr: [HTMLDivElement] | []) => [...oldArr, rowItem]);
+      });
+    });
+  };
+
+  window.addEventListener("keydown", (e: { key: string }) => {
+    const keysStrings: string[] = [];
+    keys.forEach((key: HTMLDivElement) => {
+      keysStrings.push(key.innerText);
+      key.classList.remove("currentKey");
+    });
+
+    const currentKey: number = keysStrings.indexOf(e.key);
+    if (currentKey !== -1) {
+      keys[currentKey].classList.add("currentKey");
+      setTimeout(() => {
+        keys[currentKey].classList.remove("currentKey");
+      }, 100);
+    }
+  });
+
   return (
     <div className="keyboard">
       <p>Keyboard</p>
-      <Keys />
+      <Keys onInit={(keyboard: any) => initKeysHandler(keyboard)} />
     </div>
   );
 };
